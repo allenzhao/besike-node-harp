df059c8dc468918ab1ec3e58b18cbc676f79e0af
Allen Zhao <cnallenzhao@gmail.com>
2015-01-14 02:23:14 +0800

    Rendering Jade Templates

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+var miniHarpHandler = function(req, root) {
+
+  var extname = path.extname(req.url);
+  if (extname === ".html"){
+    makeJade(root + req.url);
+  }
+}
+
 var miniharp = function(root) {
   var connect = require('connect');
   var serveStatic = require('serve-static');
+  var path = require('path');
+  var makeJade = require('./lib/processor/jade');
   var mHarp = connect();
   mHarp.use(serveStatic(root));
+  mHarp.use(makeJade(root));
   return mHarp;
 }
 
