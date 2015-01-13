e53d9bb289560cae2efb1012e197c0f370de467e
Allen Zhao <cnallenzhao@gmail.com>
2015-01-14 02:30:59 +0800

    Rendering Less Templates

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var miniHarpHandler = function(req, root) {
-
-  var extname = path.extname(req.url);
-  if (extname === ".html"){
-    makeJade(root + req.url);
-  }
-}
-
 var miniharp = function(root) {
   var connect = require('connect');
   var serveStatic = require('serve-static');
   var path = require('path');
   var makeJade = require('./lib/processor/jade');
+  var makeLess = require('./lib/processor/less');
   var mHarp = connect();
   mHarp.use(serveStatic(root));
   mHarp.use(makeJade(root));
+  mHarp.use(makeLess(root));
   return mHarp;
 }
 
