5e3e91408cab4e1f7c45e1f3eaa39320dcce9192
Allen Zhao <cnallenzhao@gmail.com>
2015-01-14 22:52:07 +0800

    Finish mini-harp-preprocessor

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var miniharp = function(root) {
-  var connect = require('connect');
-  var serveStatic = require('serve-static');
-  var path = require('path');
-  var makeJade = require('./lib/processor/jade');
-  var makeLess = require('./lib/processor/less');
-  var rewriteURL = require('./lib/URLrewrite');
-  var mHarp = connect();
-  mHarp.use(rewriteURL());
-  mHarp.use(serveStatic(root));
-  mHarp.use(makeJade(root));
-  mHarp.use(makeLess(root));
-  return mHarp;
-}
+var miniharp = function (root) {
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+    var path = require('path');
+    var makeJade = require('./lib/processor/jade');
+    var makeLess = require('./lib/processor/less');
+    var rewriteURL = require('./lib/URLrewrite');
+    var mHarp = connect();
+    mHarp.use(rewriteURL());
+    mHarp.use(serveStatic(root));
+    mHarp.use(makeJade(root));
+    mHarp.use(makeLess(root));
+    return mHarp;
+};
 
 module.exports = miniharp;
