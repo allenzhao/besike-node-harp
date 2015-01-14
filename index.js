78c0af61b2ca9a370ee6b7b4befb1aed9b1aa47c
Allen Zhao <cnallenzhao@gmail.com>
2015-01-14 10:32:31 +0800

    Serving The Root Path

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var miniharp = function(root) {
   var path = require('path');
   var makeJade = require('./lib/processor/jade');
   var makeLess = require('./lib/processor/less');
+  var rewriteURL = require('./lib/URLrewrite');
   var mHarp = connect();
+  mHarp.use(rewriteURL());
   mHarp.use(serveStatic(root));
   mHarp.use(makeJade(root));
   mHarp.use(makeLess(root));
